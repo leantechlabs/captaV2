d73c864a608e51a8992b5d7c8c9a86fa17c1de49
unknown <msdkiran0721@gmail.com>
2023-10-17 19:23:01 +0530

    committed

---
+++ client/src/pages/ScriptSection.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const ScriptSection = () => {
+  return (
+    <div>
+      <script src="/js/core/popper.min.js"></script>
+        <script src="/js/core/bootstrap.min.js"></script>
+        <script src="/js/plugins/perfect-scrollbar.min.js"></script>
+        <script src="/js/plugins/smooth-scrollbar.min.js"></script>
+        <script src='/js/plugins/smooth.js'></script>
+        <script async defer src="https://buttons.github.io/buttons.js"></script>
+        <script src="/js/argon-dashboard.min.js?v=2.0.4"></script>
+    </div>
+  )
+}
+
+export default ScriptSection
---
+++ client/src/pages/SignIn.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+import Layout from './Layout';
+import ScriptSection from './ScriptSection';
+import Form from './Form';
+
+const SignIn = () => {
+  const [message , setMessage] = useState('');
+
+  const updateMessage = () => {
+    setMessage('Invalid email and password');
+  };
+  
+
+  return (
+    <html lang="en">
+     <Layout/>
+      <body className="">
+        <main className="main-content  mt-0">
+          <section>
+            <div className="page-header min-vh-100">
+              <div className="container">
+                <div className="row">
+                  <div className="col-xl-4 col-lg-5 col-md-7 d-flex flex-column mx-lg-0 mx-auto">
+                    <div className="card card-plain">
+                      <div className="card-header pb-0 text-start">
+                        <h4 className="font-weight-bolder">Sign In</h4>
+                        <p className="mb-0">Welcome back Captain !.</p>
+                      </div>
+                      <div className="card-body">
+                        <Form  message={message} updateMessage={updateMessage}/>
+                      </div>
+                      <div className="card-footer text-center pt-0 px-lg-2 px-1">
+                        <p className="mb-4 text-sm mx-auto">
+                          Forgot your password?
+                          <a href="javascript:;" className="text-primary text-gradient font-weight-bold">
+                            Reset
+                          </a>
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-6 d-lg-flex d-none h-100 my-auto pe-0 position-absolute top-0 end-0 text-center justify-content-center flex-column">
+                    <div className="position-relative bg-gradient-primary h-100 m-3 px-7 border-radius-lg d-flex flex-column justify-content-center overflow-hidden">
+                      <span style={{ backgroundColor: 'white' }} className="mask opacity-7">
+                        <img src="img/logo.png" className="logo-style" alt="Logo" />
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+        </main>
+        <ScriptSection/>
+      </body>
+    </html>
+  );
+};
+
+export default SignIn;
