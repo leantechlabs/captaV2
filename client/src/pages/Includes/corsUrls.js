f45f874391dcd20cf7c98e1510aae7ddcd9cf243
leantechlabs <info.leantechlabs@gmail.com>
2023-11-15 00:24:43 +0530

    Update corsUrls.js

--- client/src/pages/Includes/corsUrls.js
+++ client/src/pages/Includes/corsUrls.js
@@ -1,8 +1,8 @@
-const flag= 'Test'
+const flag= 'Dev'
 var Api= 'http://localhost:3001' //testAPi
 if (flag==='Dev'){
     //const Api= 'https://ap-south-1.console.aws.amazon.com/codesuite/codepipeline/pipeline/' //Production APi
-     Api= 'https://capta-server.vercel.app'
+     Api= 'http://13.200.80.205:3001/'
 }
 const ApiUrls= {
     'api':Api,
